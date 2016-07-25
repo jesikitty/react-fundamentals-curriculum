42ba98ba8deaae61ea01af97b825046e2c3ad812
Jesi Merrick <jmerrick@digitalprimates.local>
2016-07-25 15:30:37 -0500

    step 3: ui for home

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,3 +1,9 @@
 import React from 'react';
+import styles from '../main.css';
 
-export default () => <div>Hello World!</div>;
+export default (props) => (
+	<div className='container-fluid'>
+		<header><h1>Main.js Header</h1></header>
+		{props.children}
+	</div>
+);
